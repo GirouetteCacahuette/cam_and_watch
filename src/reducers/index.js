ad00eb9a4ce4205644e00a7e43c3a188600f6b83
Martin Prieur <martin.prieur@outlook.fr>
2019-07-31 16:20:12 +0200

    feat(webapp): configure redux store

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { camAndWatchReducer } from './camAndWatchReducer';
+
+export const combinedReducers = combineReducers({
+    camAndWatchReducer
+});
