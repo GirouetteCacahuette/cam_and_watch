7a4ec190a5b1423de14d66fbf8f60ef396584109
Martin Prieur <martin.prieur@outlook.fr>
2019-08-05 17:07:19 +0200

    feat(webapp): add function to get keypoint coordinates from a posenet pose object

--- src/config/Posenet/functions.posenet.config.js
+++ src/config/Posenet/functions.posenet.config.js
@@ -17,3 +17,16 @@ export const getPose = async (net, mediaElementId, config = {}) => {
         ...config
     });
 };
+
+/**
+ * Returns coordinates of given body part (keypoint) using a pose object from posenet as input
+ * @param pose {Object} : pose from posenet
+ * @param part {String} : body part to find
+ * @param minimumAccuracy {number} : wanted minimum accuracy for the given part, null returned if not satisfied
+ * @returns {{ x: number, y: number}|null} : null if part not found or if minimum accuracy not satisfied, { x, y }
+ * object otherwise
+ */
+export const getKeypoint = (pose, part, minimumAccuracy = POSENET_CONSTANTS.PART_MINIMUM_ACCURACY) => {
+    const keypoint = pose.keypoints.find(keypoint => keypoint.part === part && keypoint.score >= minimumAccuracy);
+    return keypoint ? keypoint.position : null;
+};
